3afde3fd056dd3974a9436f3abc5c692d92bc8f5
Ilia Zakharchenia <iliazakh97@gmail.com>
2023-10-01 18:50:40 +0200

    Enable configuration properties for rsa

--- src/main/java/dev/lxqtpr/lindaSocialMedia/LindaSocialMediaApplication.java
+++ src/main/java/dev/lxqtpr/lindaSocialMedia/LindaSocialMediaApplication.java
@@ -1,11 +1,14 @@
 package dev.lxqtpr.lindaSocialMedia;
 
+import dev.lxqtpr.lindaSocialMedia.Auth.Configs.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class LindaSocialMediaApplication {
 
 	public static void main(String[] args) {
