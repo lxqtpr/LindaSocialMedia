ab7cb48576dc4065daf446ce375c786ba3f939c9
Ilia Zakharchenia <iliazakh97@gmail.com>
2023-10-03 22:11:18 +0200

    Revert commit

--- src/main/java/dev/lxqtpr/lindaSocialMedia/Domain/Artist/ArtistRepository.java
+++ src/main/java/dev/lxqtpr/lindaSocialMedia/Domain/Artist/ArtistRepository.java
@@ -1,11 +1,6 @@
 package dev.lxqtpr.lindaSocialMedia.Domain.Artist;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.ListPagingAndSortingRepository;
 
-public interface ArtistRepository extends
-        JpaRepository<ArtistEntity, Long>,
-        ListPagingAndSortingRepository<ArtistEntity,Long>
-{
-
+public interface ArtistRepository extends JpaRepository<ArtistEntity, Long> {
 }
